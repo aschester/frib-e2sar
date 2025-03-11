a16b0906d3b4c7c044e2ef8f54222652801a621d
giordano cerizza <cerizza@nscl.msu.edu>
2025-03-11 17:21:26 -0400

    add command line args for running evb

--- scripts/run_evb.py
+++ scripts/run_evb.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
 ##
-# @file run_evb.py
-# @details Run event builder for FRIB-E2SAR workflow.
-# @note Deprecated 3/7/25: use run_combined.py instead.
+# @file run_combined.py
+# @details Run EVB pipe and eventlog for FRIB-E2SAR workflows.
 #
 
+## @todo (ASC 3/7/25): Setup all recv/sorters with one evb framework app.
+
+##
+# @note (ASC 3/11/25): EVB on localhost, which may be different than the
+# Reassembly and sorting host machine(s). See setup_evb.sh where ring sources
+# for the event building are defined.
+#
+
+import argparse
 import os
 import shlex
 import signal
@@ -19,6 +27,53 @@ def handler(signum, frame):
     print(f"\nSignal handler called with signal {signame} ({signum})")
     sys.exit(0)
 
+# Creates the sink ring if it doesnt already exist:
+def create_sink(sink_name):
+    daqbin = os.getenv("DAQBIN")
+    cmd = f"{daqbin}/ringbuffer list"
+    proc = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
+    try:
+        proc.check_returncode() # This really ought never to fail...
+    except subprocess.CalledProcessError as e:
+        print(f"ERROR: {e} {proc.stdout} {proc.stdin}")
+        sys.exit(1) # Perhaps not a clean exit?
+    else:
+        ringbuffers = proc.stdout.split("\n")
+        if ringbuffers[-1] == "":
+            ringbuffers = ringbuffers[:-1]            
+        if sink_name in ringbuffers:
+            print(f"ringbuffer sink {sink_name} already exists, skipping")
+            return
+        else:
+            print(f"creating ringbuffer sink {sink_name}")  
+            cmd = f"{daqbin}/ringbuffer create {sink_name}"
+            proc = subprocess.run(shlex.split(cmd), capture_output=True,
+                                  text=True)
+            try:
+                proc.check_returncode() # This really ought never to fail...
+            except subprocess.CalledProcessError as e:
+                print(f"ERROR: {e} {proc.stdout} {proc.stdin}")
+                sys.exit(1) # Perhaps not a clean exit?
+                
+parser = argparse.ArgumentParser(
+    prog="run_evb.py",
+    description="Run EVB pipe and eventlog for FRIB-E2SAR workflows",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+parser.add_argument("-c", "--config-file",
+                    help="path to EVB configuration file",
+                    default=f"{os.getcwd()}/setup_evb.sh")
+parser.add_argument("-n", "--number-of-sources",
+                    help="number of data sources",
+                    default=1)
+parser.add_argument("-S", "--evbring",
+                    help="ringbuffer sink for built event data (localhost)",
+                    default="frib_e2sar_evb")
+parser.add_argument("--glomdt",
+                    help="correlation window for evt building in nanoseconds",
+                    default=1000)
+args = parser.parse_args()
+    
 signal.signal(signal.SIGINT, handler)
 
 # NSCLDAQ needed to create evb sources:
@@ -28,23 +83,53 @@ if daqbin is None:
     sys.exit(1)
 else:
     print(f"Using DAQBIN: {daqbin}")
-    
+
 ##
-# Event building:
+# Event builder:
 #
 
-path = os.path.dirname(os.path.realpath(__file__))
-cmd = f"{path}/setup_evb.sh"
-args = shlex.split(cmd)
-print(f"Running evtbuild command: {cmd}")
+evb_cmd = f"{args.config_file} -sink {args.evbring} -window {args.glomdt}"
+evb_args = shlex.split(evb_cmd)
+print(f"Running evtbuild command: {evb_cmd}")
+    
+##
+# eventlog to write data to disk. Note that this will write data
+# into the current working directory:
+#
 
-proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+evtlog_cmd = f"{daqbin}/eventlog -s tcp://localhost/{args.evbring} " \
+    f"--number-of-sources={args.number_of_sources} --oneshot"
+evtlog_args = shlex.split(evtlog_cmd)
+print(f"Running eventlog command: {evtlog_cmd}")
 
-while proc.poll() == None:
-    time.sleep(1)
+##
+# Processing loop:
+#
 
-if proc.returncode:
-    print(f"ERROR: process terminated with returncode {proc.returncode}")
-    proc.kill()
-    stdout, _ = proc.communicate()
-    print(f"{stdout.decode('utf-8')}")
+while True:
+    create_sink(args.evbring) # Make sure the sink exists
+    evb_proc = subprocess.Popen(evb_args, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+    print("Recording run...",end="")
+    sys.stdout.flush()
+    evtlog_proc = subprocess.run(evtlog_args, capture_output=True, text=True)
+    
+    try:
+        evtlog_proc.check_returncode()
+    except subprocess.CalledProcessError as e:
+        print(f"ERROR: {e} {evtlog_proc.stdout} {evtlog_proc.stderr}")
+        print("Killing evb...")
+        evb_proc.kill()
+        sys.exit(1)
+    else:
+        print("done")
+        sys.stdout.flush()
+        print(f"{evtlog_cmd} completed with returncode " \
+              f"{evtlog_proc.returncode}")
+        time.sleep(2)
+        evb_proc.kill()
+        evb_proc.wait()
+        print(f"{evb_cmd} completed with returncode "\
+              f"{evb_proc.returncode}")
+        time.sleep(2)
+        print("Restarting evb pipeline...")
